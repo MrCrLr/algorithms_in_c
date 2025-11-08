398387da4ae3886c349ba65d4f5b528d1075c701
Nicholas Stevens <nichstev@gmail.com>
2025-11-08 18:21:18 +0100

    Add iterative postorder traversal exercise

--- ch05_recursion/iterative_postorder_trav.c
+++ ch05_recursion/iterative_postorder_trav.c
@@ -3,28 +3,122 @@
 // Recursive Postorder Traversal 
 /*--------------------------------------------------------------------------*/
 traverse(node *t) {
-    if (t != z) {
+    if (t != z) 
+    {
         traverse(t->l);
         traverse(t->r);
         visit(t)
     }
 }
 /*--------------------------------------------------------------------------*/
-// Step 1:  
+// Step 1: First sketch... incorrect.
 /*--------------------------------------------------------------------------*/
-traverse(node *t) {} 
+traverse(node *t) { 
+    l:  if (!t) goto s;
+        push(t);
+        t = t->l;
+
+    r:  if (!t->r) goto s;
+        t = t->r;
+        push(t);
+        if !(t->l) goto s;
+        t = t->l;
+        goto l;
+
+    s:  if (stack_empty) goto x;
+        t = pop(t);
+        visit(t);
+        goto r;
+
+    x: ;
+}
 
 /*--------------------------------------------------------------------------*/
-// Step 2: 
+// Step 1: Visited plays role of “returning from left, now go right.
+// 4 Control Points:
+// l:  going down left
+// r:  going right
+// s:  stack handling (decisions)
+// x:  exit
 /*--------------------------------------------------------------------------*/
-traverse(node *t) {}
+// stack entries: (node*, visited)
+traverse(node *t) { 
+l:  
+    while (t != z) {
+        push_state(t, 0);
+        t = t->l;
+    }
+
+s:
+    if (stack_empty()) goto x;
+    
+    pair top = peek();
+    if (top.visited == 0) {
+        set_top_state(1);       // mark: left done, right next
+        t = top.node->r;
+        goto r;
+    }
+    // top.visited == 1 --> both subtrees done
+    pop();
+    visit(top.node);
+    goto s;
+
+r:
+    goto l;
+
+x: ;
+}
 
 /*--------------------------------------------------------------------------*/
-// Step 3: 
+// Step 2: Remove obviously redundant "goto r" control point
 /*--------------------------------------------------------------------------*/
-traverse(node *t) {}
+traverse(node *t) { 
+l:  
+    while (t != z) {
+        push_state(t, 0);
+        t = t->l;
+    }
+s:
+    if (stack_empty()) goto x;
+    
+    pair top = peek();
+    if (top.visited == 0) {
+        set_top_state(1);       // mark: left done, right next
+        t = top.node->r;
+        goto l;
+    }
+    // top.visited == 1 --> both subtrees done
+    pop();
+    visit(top.node);
+    goto s;
+
+x: ;
+}
 
 /*--------------------------------------------------------------------------*/
-// Step 4: 
+// Step 3: Iterative Postorder
+// a) Outer loop replaces bounce between s: and x:.
+// b) Inner loop replaces l: (left descent).
+// c) If/else replaces the branching at s: and the jump to r:.
+// d) t = z after visiting keeps control from re-descending into same node.
 /*--------------------------------------------------------------------------*/
-traverse(node *t) {}
+traverse(node *t) {
+   
+    while (t != z || stack_empty()) {
+        while (t != z) {
+            push_state(t, 0);
+            t = t->l;
+        }
+
+        pair top = peek();
+
+        if (top.visited == 0) {
+            set_top_state(1);
+            t = top.node->r;
+        } else {
+            pop();
+            visit(top.node);
+            t = z; // don't re-enter left loop on same node
+        }
+    }
+}
--- ch05_recursion/iterative_preorder_traversal.c
+++ ch05_recursion/iterative_preorder_traversal.c
@@ -1,5 +1,24 @@
 /*--------------------------------------------------------------------------*/
-// Standard non-recursive tree traversal method (Sedgewick)
+// Recusive preorder traversal (Sedgewick)
+/*--------------------------------------------------------------------------*/
+traverse(struct node *t) 
+{
+    push(t);
+
+    while (!stack_empty()) 
+    {
+        t = pop();
+        visit(t);
+
+        if (t->r  != z) 
+            push(t->r);
+        if (t->l  != z) 
+            push(t->l);
+    }
+}
+
+/*--------------------------------------------------------------------------*/
+// Iterative preorder traversal (Sedgewick)
 /*--------------------------------------------------------------------------*/
 traverse(struct node *t) 
 {
@@ -15,24 +34,5 @@ traverse(struct node *t)
             push(t->r);
             push(t->l);
         }
-    }
-}
-
-/*--------------------------------------------------------------------------*/
-// Again for review, the recusive preorder traversal (Sedgewick)
-/*--------------------------------------------------------------------------*/
-traverse(struct node *t) 
-{
-    push(t);
-
-    while (!stack_empty()) 
-    {
-        t = pop();
-        visit(t);
-
-        if (t->r  != z) 
-            push(t->r);
-        if (t->l  != z) 
-            push(t->l);
     }
 }
